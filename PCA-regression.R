ab922a1c6b205adc19c8442893e4fd08a410ac4d
DoronGurel <dorongurel@gmail.com>
2018-11-12 14:21:03 +0200

    s

--- PCA-regression.R
+++ PCA-regression.R
@@ -1,4 +1,3 @@
-
 ########### Training data (rankings only, no dates):
 con = url("http://www.tau.ac.il/~saharon/StatsLearn2018/train_ratings_all.dat")
 X.tr = read.table (con)
@@ -29,6 +28,7 @@ trtr = data.frame(X = X.tr[-va.id,],y=y.tr[-va.id,])
 #va = data.frame (X = X.tr[va.id,], yda=y.da.tr[va.id,], y=y.tr[va.id,]) #include dates
 va = data.frame (X = X.tr[va.id,],y=y.tr[va.id,])
 
+
 # fill missing values with column mean
 X.tr_center <- apply(X.tr,2,function(x) mean(x[x!=0]))
 X.tr_full<-X.tr
@@ -57,36 +57,57 @@ min(rmse)
 # do PCA and train linear regression on PCA -  adding columns by correlation
 rmse = c()
 train.pca = prcomp(X.tr_full[-va.id,], center = TRUE,scale. = TRUE)
+df.trtr.pca = data.frame(X = train.pca$x ,Y = y.tr[-va.id,])
+add_to_model = which.max(apply(df.trtr.pca[,-100],2,function(x) abs(cor(x,df.trtr.pca['Y']))))
+model_params = as.vector(add_to_model)
+new_train = df.trtr.pca[,c(100, model_params)]
+model = lm(Y~.,data = new_train)
+
 va.pca <- scale(X.tr_full[va.id,], center= train.pca$center)
 va.pca <- va.pca %*% train.pca$rotation
-for(k in 1:50){
-  df.trtr.pca = data.frame(X = train.pca$x[,1:k],Y = y.tr[-va.id,])
-  pca_lin_mod = lm(Y~., data = df.trtr.pca)
-  
-  df.va.pca = data.frame(X = va.pca[,1:k],Y = y.tr[va.id,])
-  
-  lin.pred = predict (pca_lin_mod, newdata=df.va.pca)
-  rmse = append(rmse,sqrt(mean((df.va.pca$Y-lin.pred)^2)))
-}
-
-df.trtr.pca = data.frame(X = train.pca$x[,1:k],Y = y.tr[-va.id,])
-add_to_model = which.max(apply(df.trtr.pca[,-100],2,function(x) abs(cor(x,trtr['Y']))))
-model_params = as.vector(add_to_model)
-new_train = trtr[,c(100, model_params)]
-model = lm(y~.,data = new_train)
-lin.pred = predict (model, newdata=va)
-rmse = append(rmse,sqrt(mean((va$y-lin.pred)^2)) )
-ignore_params = c()
+df.va.pca <- data.frame(X = va.pca ,Y = y.tr[va.id,])
+lin.pred = predict (model, newdata=df.va.pca)
+rmse = append(rmse,sqrt(mean((df.va.pca$Y-lin.pred)^2)) )
 for(i in 1:99){
-  add_to_model = as.numeric(substr(names(which.max(apply(trtr[,-c(100,model_params)],2,function(x) abs(cor(x,model$residuals))))),4,10))
+  add_to_model = as.numeric(substr(names(which.max(apply(df.trtr.pca[,-c(100,model_params)],2,function(x) abs(cor(x,model$residuals))))),5,10))
   model_params = as.vector(append(model_params,add_to_model))
-  new_train = trtr[,c(100, model_params)]
-  model = lm(y~.,data = new_train)
+  new_train = df.trtr.pca[,c(100, model_params)]
+  model = lm(Y~.,data = new_train)
   
-  lin.pred = predict (model, newdata=va)
+  lin.pred = predict (model, newdata=df.va.pca)
   rmse = append(rmse,sqrt(mean((va$y-lin.pred)^2)) )
 }
 
 plot(rmse)
+min(rmse)
+
+
+
+# do PCA and train linear regression on PCA -  adding columns by correlation - indicator(is_early)
+rmse = c()
+is_early = 1997 + y.da.tr[,1]/365 - 2000 <= 3
+
+train.pca = prcomp(X.tr_full[-va.id,][is_early[-va.id],], center = TRUE,scale. = TRUE)
+df.trtr.pca = data.frame(X = train.pca$x ,Y = y.tr[-va.id,][is_early[-va.id]])
+add_to_model = which.max(apply(df.trtr.pca[,-100],2,function(x) abs(cor(x,df.trtr.pca['Y']))))
+model_params = as.vector(add_to_model)
+new_train = df.trtr.pca[,c(100, model_params)]
+model = lm(Y~.,data = new_train)
+
+va.pca <- scale(X.tr_full[va.id,][is_early[va.id],], center= train.pca$center)
+va.pca <- va.pca %*% train.pca$rotation
+df.va.pca <- data.frame(X = va.pca ,Y = y.tr[va.id,][is_early[va.id]])
+lin.pred = predict (model, newdata=df.va.pca)
+rmse = append(rmse,sqrt(mean((df.va.pca$Y-lin.pred)^2)) )
+for(i in 1:99){
+  add_to_model = as.numeric(substr(names(which.max(apply(df.trtr.pca[,-c(100,model_params)],2,function(x) abs(cor(x,model$residuals))))),5,10))
+  model_params = as.vector(append(model_params,add_to_model))
+  new_train = df.trtr.pca[,c(100, model_params)]
+  model = lm(Y~.,data = new_train)
+  
+  lin.pred = predict (model, newdata=df.va.pca)
+  rmse = append(rmse,sqrt(mean((va$y-lin.pred)^2)) )
+}
+
 plot(rmse)
 min(rmse)

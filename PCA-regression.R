1c03048434c0f5f49ed03da0285203483a13a10f
DoronGurel <dorongurel@gmail.com>
2018-11-14 10:17:05 +0200

    pca update

--- PCA-regression.R
+++ PCA-regression.R
@@ -75,17 +75,18 @@ for(i in 1:99){
   model = lm(Y~.,data = new_train)
   
   lin.pred = predict (model, newdata=df.va.pca)
-  rmse = append(rmse,sqrt(mean((va$y-lin.pred)^2)) )
+  rmse = append(rmse,sqrt(mean((df.va.pca$Y-lin.pred)^2)) )
 }
 
 plot(rmse)
 min(rmse)
+which.min(rmse)
 
 
 
 # do PCA and train linear regression on PCA -  adding columns by correlation - indicator(is_early)
 rmse = c()
-is_early = 1997 + y.da.tr[,1]/365 - 2000 <= 3
+is_early_tr = 1997 + y.da.tr[,1]/365 - 2000 <= 3
 
 train.pca = prcomp(X.tr_full[-va.id,][is_early[-va.id],], center = TRUE,scale. = TRUE)
 df.trtr.pca = data.frame(X = train.pca$x ,Y = y.tr[-va.id,][is_early[-va.id]])
@@ -106,8 +107,94 @@ for(i in 1:99){
   model = lm(Y~.,data = new_train)
   
   lin.pred = predict (model, newdata=df.va.pca)
-  rmse = append(rmse,sqrt(mean((va$y-lin.pred)^2)) )
+  rmse = append(rmse,sqrt(mean((df.va.pca$Y-lin.pred)^2)) )
 }
 
 plot(rmse)
 min(rmse)
+
+
+
+# do PCA and train linear regression on PCA -  adding columns by correlation - time_to_review
+results_20 = c()
+results_25 = c()
+results_30 = c()
+results_35 = c()
+results_40 = c()
+num_predictors = c()
+for(validation in 1:100){
+  va.id = sample (n,nva) # choose 2000 points for validation
+  X.trtr = X.tr[-va.id,]
+  X.trva = X.tr[va.id,]
+  X.trtr_center <- apply(X.trtr,2,function(x) mean(x[x!=0]))
+  X.tr_full<-X.trtr
+  for(c in 1:ncol(X.tr)){
+    X.tr_full[,c][X.tr_full[,c] == 0] <- X.trtr_center[c]
+  }
+  X.trva_center <- apply(X.trva,2,function(x) mean(x[x!=0]))
+  X.trva_full<-X.trva
+  for(c in 1:ncol(X.tr)){
+    X.trva_full[,c][X.trva_full[,c] == 0] <- X.trva_center[c]
+  }
+  rmse = c()
+  time_to_review = (y.da.tr[,1] - min(y.da.tr[,1]))/max(y.da.tr[,1])
+#  time_to_review = (time_to_review-mean(time_to_review))/sd(time_to_review)
+  train.pca = prcomp(X.tr_full, center = FALSE,scale. = TRUE)
+  df.trtr.pca = data.frame(X = train.pca$x, TTR = time_to_review[-va.id], Y = y.tr[-va.id,])
+  add_to_model = which.max(apply(df.trtr.pca[,-c(101,100)],2,function(x) abs(cor(x,df.trtr.pca['Y']))))
+  model_params = as.vector(add_to_model)
+  new_train = df.trtr.pca[,c(101,100, model_params)]
+  model = lm(Y~TTR*(1+.),data = new_train)
+  
+  va.pca <- scale(X.trva_full, center= train.pca$center)
+  va.pca <- va.pca %*% train.pca$rotation
+  df.va.pca <- data.frame(X = va.pca ,TTR = time_to_review[va.id], Y = y.tr[va.id,])
+  lin.pred = predict (model, newdata=df.va.pca)
+  rmse = append(rmse,sqrt(mean((df.va.pca$Y-lin.pred)^2)) )
+  for(i in 1:97){
+    add_to_model = as.numeric(substr(names(which.max(apply(df.trtr.pca[,-c(101,100,model_params)],2,function(x) abs(cor(x,model$residuals))))),5,10))
+    model_params = as.vector(append(model_params,add_to_model))
+    new_train = df.trtr.pca[,c(101,100, model_params)]
+    model = lm(Y~TTR*(1+.),data = new_train)
+    
+    lin.pred = predict (model, newdata=df.va.pca)
+    rmse = append(rmse,sqrt(mean((y.tr[va.id,]-lin.pred)^2)) )
+  }
+  
+  plot(rmse)
+  print(min(rmse))
+  num_predictors = append(num_predictors,which.min(rmse))
+  results_20 = append(results_20,rmse[20])
+  results_25 = append(results_25,rmse[25])
+  results_30 = append(results_30,rmse[30])
+  results_35 = append(results_35,rmse[35])
+  results_40 = append(results_40,rmse[40])
+}
+
+
+# do PCA and KNN
+rmse = c()
+train.pca = prcomp(X.tr_full[-va.id,], center = TRUE,scale. = TRUE)
+df.trtr.pca = data.frame(X = train.pca$x, TTR = time_to_review[-va.id], Y = y.tr[-va.id,])
+cos.sim <- function(A,B) 
+{
+  return( sum(A*B)/sqrt(sum(A^2)*sum(B^2)) )
+}  
+va.pca <- scale(X.tr_full[va.id,], center= train.pca$center)
+va.pca <- va.pca %*% train.pca$rotation
+nval<- nrow(va.pca)
+
+
+
+rmse = c()
+for(k in c(10)){
+  predictions<-rep(NA,nval)
+  for(i in 1:nval){
+    similarities <- apply(df.trtr.pca[,1:10],1, function(x) cos.sim(x,va.pca[i,1:10]))
+    predictions[i]<-mean(df.trtr.pca[which(similarities %in% sort(similarities, decreasing=F)[1:k]),"Y"])
+  }
+  rmse<- append(rmse, sqrt(mean((va$y-predictions)^2)))
+  print(k)
+}
+plot(rmse)
+
